83a45180cd57d465802eee03663f2499775a05a3
Pedro Sbardelotto <sbardelottopedro97@gmail.com>
2024-11-12 12:57:52 -0300

    test

--- Node-Express-MongoDB/server.js
+++ Node-Express-MongoDB/server.js
@@ -1,18 +1,8 @@
-// import http from "http";
-import app from "./src/app"
+
+import app from "./src/app.js"
 const PORT = 3000;
 
-const rotas = {
-    "/": "Express APi ",
-    "/Livros": "Entrei na rota livros",
-    "/Autores": "Entrei na rota aturores"
-}
 
-const server = http.createServer((req, res)=>{
-    res.writeHead(200, {"content-type": "text/plain"});
-    res.end(rotas[req.url]);
-});
-
-server.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log("servidor escutando!");
 });
--- Node-Express-MongoDB/src/app.js
+++ Node-Express-MongoDB/src/app.js
@@ -1,8 +1,23 @@
 import express from "express";
+
 const app = express();
 
-app.get("/",(req, res)=> {
-    req.status(200).send(" curso de node.js");
+const livros = [
+    {
+        id: 1,
+        titulo: " O Senhor Dos Anéis"
+    },
+    {
+        id: 2,
+        titulo: " O Hobbit"
+    }
+]
+app.get("/", (req, res) => {
+    res.status(200).send(" curso de node.js");
+});
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros);
 });
 
 export default app;
