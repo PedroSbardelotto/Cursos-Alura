f5d37de09d69c7cb6fbb2ef157689deb3ee1c0dd
Pedro Sbardelotto <sbardelottopedro97@gmail.com>
2024-11-12 16:11:26 -0300

    rotas finalizadas, proximos passo implementar a connect com o mongoDB

--- Node-Express-MongoDB/src/app.js
+++ Node-Express-MongoDB/src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 
 const app = express();
-
+app.use(express.json()); //MIDDLEWARE 
 const livros = [
     {
         id: 1,
@@ -12,6 +12,13 @@ const livros = [
         titulo: " O Hobbit"
     }
 ]
+
+function buscaLivro(id) {
+    return livros.findIndex(livros => {
+        return livros.id === Number(id);
+    })
+}
+
 app.get("/", (req, res) => {
     res.status(200).send(" curso de node.js");
 });
@@ -20,4 +27,29 @@ app.get("/livros", (req, res) => {
     res.status(200).json(livros);
 });
 
+app.get("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    res.status(200).json(livros[index]);
+});
+
+app.post("/livros", (req, res) => {
+    livros.push(req.body);
+    res.status(201).send("livro cadastrado com sucesso")
+});
+
+app.put("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.status(200).json(livros);
+});
+
+app.delete("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros.splice(index, 1);
+    res.status(200).send("Livro removido com sucesso!");
+});
+
 export default app;
+
+
+
